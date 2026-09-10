8730590ea75116846143effe2bfe4075ccd7b35c
Marko Smiljanic <marko.smiljanic313@gmail.com>
2026-09-10 20:53:00 +0200

    Finalize CONFIG resolution refactor

---
+++ src/dendroflow/configuration/resolution/__init__.py
@@ -0,0 +1,1 @@
+"""Internal CONFIG resolution implementation."""
--- src/dendroflow/configuration/resolver.py
+++ src/dendroflow/configuration/resolver.py
@@ -1,3 +1,5 @@
+"""Compatibility façade for CONFIG resolution."""
+
 from .resolution.aliases import (
     AliasRegistry as _AliasRegistry,
 )
@@ -52,17 +54,27 @@ DeclarationAlias = _DeclarationAlias
 ReferenceAlias = _ReferenceAlias
 build_alias_registry = _build_alias_registry
 
-resolve_deployment_declarations = _resolve_deployment_declarations
-resolve_deployment_reference_aliases = _resolve_deployment_reference_aliases
-resolve_location_declarations = _resolve_location_declarations
-resolve_location_label_declarations = _resolve_location_label_declarations
-resolve_location_reference_aliases = _resolve_location_reference_aliases
-resolve_sensor_declarations = _resolve_sensor_declarations
-resolve_sensor_model_declarations = _resolve_sensor_model_declarations
-resolve_sensor_model_reference_aliases = _resolve_sensor_model_reference_aliases
-resolve_sensor_reference_aliases = _resolve_sensor_reference_aliases
-resolve_simple_declarations = _resolve_simple_declarations
 resolve_simple_reference_aliases = _resolve_simple_reference_aliases
+resolve_simple_declarations = _resolve_simple_declarations
+
+resolve_sensor_model_reference_aliases = (
+    _resolve_sensor_model_reference_aliases
+)
+resolve_sensor_model_declarations = _resolve_sensor_model_declarations
+
+resolve_sensor_reference_aliases = _resolve_sensor_reference_aliases
+resolve_sensor_declarations = _resolve_sensor_declarations
+
+resolve_location_reference_aliases = _resolve_location_reference_aliases
+resolve_location_declarations = _resolve_location_declarations
+resolve_location_label_declarations = (
+    _resolve_location_label_declarations
+)
+
+resolve_deployment_reference_aliases = (
+    _resolve_deployment_reference_aliases
+)
+resolve_deployment_declarations = _resolve_deployment_declarations
 
 resolve_metadata_config = _resolve_metadata_config
 
